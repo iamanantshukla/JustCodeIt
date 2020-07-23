388a342dd7adcce4c7b1b61aec535ce366fafa37
Anant Shukla <anantshukla1402@gmail.com>
2020-07-23 14:46:57 +0530

    UI correction

--- app/src/main/java/com/example/yogacommunity/MainActivity.java
+++ app/src/main/java/com/example/yogacommunity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.yogacommunity;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -30,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
         mAuth= FirebaseAuth.getInstance();
 
         password=(EditText) findViewById(R.id.editPass);
